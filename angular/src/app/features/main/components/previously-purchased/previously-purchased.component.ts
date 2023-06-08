85e0e07823822f0d4532b2477c8f332d4d6582ae
talhakca <talhakca@gmail.com>
2023-06-08 13:58:48 +0300

    Previously purchased added

--- angular/src/app/features/main/components/previously-purchased/previously-purchased.component.ts
+++ angular/src/app/features/main/components/previously-purchased/previously-purchased.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { AppState } from 'src/app/features/data-stores/app-data-store/state/app-data-store.reducer';
+import { Product } from 'src/app/features/shared/sdk/models';
 
 @Component({
   selector: 'app-previously-purchased',
@@ -7,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreviouslyPurchasedComponent implements OnInit {
 
-  constructor() { }
+  subscriptions: Subscription[];
+  orderGroup: {
+    orderId: string,
+    productGroup: { quantity: number, product: Product }[]
+  }[];
+
+  constructor(
+    private store: Store<{ app: AppState }>
+  ) { }
 
   ngOnInit(): void {
+    this.subscribeToData();
+  }
+
+  subscribeToData() {
+    this.subscriptions = [
+      this.subscribeToInactiveCarts()
+    ];
+  }
+
+  subscribeToInactiveCarts() {
+    return this.store.select(state => state.app.inactiveCarts).subscribe(inactiveCarts => {
+      console.log(inactiveCarts)
+      this.orderGroup = inactiveCarts?.reduce((acc, curr) => {
+        const order: {
+          orderId: string,
+          productGroup: { quantity: number, product: Product }[]
+        } = acc?.find(item => item.orderId === curr.orderId);
+        if (order) {
+          const addedProduct = order.productGroup.find(group => group.product.id === curr.productId);
+          if (addedProduct) {
+            addedProduct.quantity++;
+          } else {
+            console.log(order)
+            order.productGroup.push({ quantity: 1, product: curr.product });
+          }
+        } else {
+          console.log(acc)
+          acc.push(
+            {
+              orderId: curr.orderId,
+              productGroup: [
+                {
+                  quantity: 1,
+                  product: curr.product
+                }
+              ]
+            }
+          );
+        }
+        return acc;
+      }, []);
+      console.log(this.orderGroup);
+    })
   }
 
 }
